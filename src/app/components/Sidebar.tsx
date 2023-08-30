c56a884770709c3bfad11eb40d49a1c34873eaef
Kauan Santos <kauansantosdepontes@gmail.com>
2023-08-30 10:23:42 -0300

    refactor: moved icons to folder src/assets and created constant of the images

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,18 +1,11 @@
-export default function Sidebar() {
-  const items = [
-    { id: 1, icon: "home.svg" },
-    { id: 2, icon: "edit.svg" },
-    { id: 3, icon: "connections.svg" },
-    { id: 4, icon: "rollback.svg" },
-    { id: 5, icon: "user.svg" },
-    { id: 6, icon: "sino.svg" },
-  ];
+import { images } from "../../constants/imagesNavList";
 
+export default function Sidebar() {
   return (
     <aside className='w-[58px] bg-blue-theme h-screen gap-8 flex fixed left-0 top-0 justify-center flex-col items-center rounded-r-[48px]'>
-      {items.map((item) => (
-        <div key={item.id}>
-          <img src={`assets/icons/${item.icon}`} className='h-8 w-8' />
+      {images.map((image) => (
+        <div key={image.id}>
+          <img src={image.icon} className='h-8 w-8' />
         </div>
       ))}
     </aside>
