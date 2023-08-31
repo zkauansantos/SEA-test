e38f34ddba78d12da263a0f5b4020fe74ab12eed
Kauan Santos <kauansantosdepontes@gmail.com>
2023-08-31 11:11:43 -0300

    feat: created calendar with date-picker for select date birthday

---
+++ src/app/components/DatePicker.tsx
@@ -0,0 +1,42 @@
+import { ptBR } from "date-fns/locale";
+import { format } from "date-fns";
+import { DayPicker } from "react-day-picker";
+
+interface DatePickerProps {
+  value: Date | undefined;
+  onChange: (date: Date) => void;
+}
+
+export default function DatePicker({ onChange, value }: DatePickerProps) {
+  return (
+    <DayPicker
+      locale={ptBR}
+      selected={value}
+      mode='single'
+      onSelect={(date) => onChange(date ?? new Date())}
+      classNames={{
+        root: 'w-full',
+        caption: "flex items-center justify-between",
+        nav: "flex gap-1",
+        nav_button_previous:
+          "!text-blue-theme flex items-center justify-center !bg-transparent",
+        nav_button_next:
+          "!text-blue-theme flex items-center justify-center !bg-transparent",
+        head_cell: "uppercase text-xs text-blue-theme font-bold pt-1 pb-2",
+        button:
+          "text-gray-700 cursor-pointer w-10 h-10 hover:bg-blue-theme/40 rounded-full",
+        day_today: "bg-gray-100 font-bold text-gray-900",
+        day_selected: "!bg-blue-theme !text-white font-medium",
+      }}
+      formatters={{
+        formatCaption: (date, options) => {
+          return (
+            <span className='text-gray-900 tracking-[-0.408px] font-medium capitalize'>
+              {format(date, "LLLL yyyy", options)}
+            </span>
+          );
+        },
+      }}
+    />
+  );
+}
---
+++ src/app/components/Form/components/DatePickerInput.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import * as Popover from "@radix-ui/react-popover";
+import cn from "../../../../utils/cn";
+import formatDate from "../../../../utils/formatDate";
+import DatePicker from "../../DatePicker";
+
+interface DatePickerInputProps {
+  className?: string;
+  error?: string;
+  value?: Date;
+  onChange: (date: Date) => void;
+}
+
+export default function DatePickerInput({
+  className,
+  error,
+  onChange,
+}: DatePickerInputProps) {
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined);
+
+  function handleChangeDate(date: Date) {
+    setSelectedDate(date);
+    onChange(date);
+  }
+
+  return (
+    <div className='w-full flex flex-col'>
+      <span>Data de Nascimento</span>
+
+      <Popover.Root>
+        <Popover.Trigger asChild>
+          <button
+            type='button'
+            className={cn(
+              "text-left outline-none border border-blue-theme w-full rounded-[10px] h-[36px] px-2",
+              error && "!border-red-900",
+              className
+            )}
+          >
+            {!!selectedDate && <span>{formatDate(selectedDate)}</span>}
+          </button>
+        </Popover.Trigger>
+
+        <Popover.Content className='z-[99] bg-white border border-blue-theme rounded-[10px] p-2 '>
+          <DatePicker
+            onChange={(date) => handleChangeDate(date)}
+            value={selectedDate}
+          />
+        </Popover.Content>
+      </Popover.Root>
+    </div>
+  );
+}
